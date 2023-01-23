8f2fd3d2757cab70a0dd64d43bc72af245b94f09
El Samp <anthony.francisco.aoct@gmail.com>
2023-01-22 22:03:02 -0500

    added churches json

--- server/server.js
+++ server/server.js
@@ -2,8 +2,35 @@ const express = require("express")
 const app = express()
 const PORT = 8000
 
-app.get("/", (request,response) => {
+
+
+let churches = [
+    {
+        "Location": "Pawtucket",
+        "Lead Pastor(s)": [{ "Samuel Francisco": "" }, { "Isabel Francisco": "" }],
+        "Associate Pastor(s)": [{ "Javier Torres": "" }],
+        "ServiceTimes": {
+            ServicioEvangelistico: { Domingos: "3:00PM" },
+            EstudioBiblico: { Martes: "7:00PM" },
+            ServicioEspecial: { Jueves: "7:30PM" }
+        },
+        "SocialMedia": {
+            Facebook:
+                "",
+            Instagram: "",
+            YouTube: "",
+        },
+        "ChurchDescitpion": "",
+
+    },
+]
+
+app.get("/", (request, response) => {
     response.sendFile(__dirname + '/admin.html')
+})
+
+app.get("/iglesias/pawtucket", (request, response) => {
+    response.json(churches)
 })
 
 app.listen(PORT, () => {
